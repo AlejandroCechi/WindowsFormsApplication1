5df0f03a9652d6d45847d79319b4c9289c595e35
alejandro daniel <alejandrocechi@hotmail.com>
2017-09-19 00:19:45 -0300

    k

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(45, 45);
+            this.button1.Location = new System.Drawing.Point(83, 76);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
